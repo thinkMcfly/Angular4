98087b1eff43e52834d9361ee6f4393b7ff36a9f
thinkMcfly <tkesten@gmail.com>
2017-06-02 11:53:32 -0500

    project has working routes. still no functionality to recipe-edit

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -26,11 +26,16 @@ export class RecipeService {
       ])
   ];
 
-  constructor(private slService: ShoppingListService) {}
+  constructor(private slService: ShoppingListService) { }
 
   getRecipes() {
     return this.recipes.slice();
   }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
